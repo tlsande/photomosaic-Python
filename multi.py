41da4bf2f6b1090321af9bd5cee3ff148b2d2717
tlsande <tlsande@g.clemson.edu>
2019-09-13 16:02:53 -0400

    Better file name creation

--- multi.py
+++ multi.py
@@ -36,13 +36,51 @@ def splitList(list):
 def processImages(locations, num):
     start = time.time()
     for i in range(0, len(locations)):
+        # Remove check from here and add it to for loop that gets locations
         if locations[i].endswith(('.png', '.jpg', '.gif')):
             img = Image.open(locations[i])
             img = img.resize((64, 64))
-            img.save(imageProcessed + "test" + str(i + num) + ".png")
+            # img.save(imageProcessed, "test", i + num, ".png")
+            # print("test", '{0:04}'.format(i+num), ".png")
+            path = imageProcessed + "test" + '{:04d}'.format(i + num) + ".png"
+            img.save(path)
     end = time.time()
     print("Runtime: " + str(end - start))
 
+def averageRGB(location):
+    img = Image.open(location).convert('RGB')
+    # Slower than numpy
+    # r = g = b = a = 0
+    # start = time.time()
+    # for x in range(img.width):
+    #     for y in range(img.height):
+    #         pixr, pixg, pixb = img.getpixel((x, y))
+    #         r += pixr
+    #         g += pixg
+    #         b += pixb
+    #         # a += pixa
+    # r = np.round(r / (img.width * img.height))
+    # g = np.round(g / (img.width * img.height))
+    # b = np.round(b / (img.width * img.height))
+    # end = time.time()
+    # print("First: ", end - start)
+    # print("(", r, ", ", g, ",", b, ")")
+
+    # start = time.time()
+    # print(np.round(np.array(img).mean(axis=(0, 1))))
+    # end = time.time()
+    # print("Second: ", end - start)
+
+    # print(np.round(np.array(img).mean(axis=(1))))
+    return np.round(np.array(img).mean(axis=(0,1)))
+    # return r, g, b
+
+def writeCache(pLocation):
+    locations = getAllPaths(pLocation)
+    # locations = getBasePaths(pLocation)
+    for i in locations:
+        print(i)
+        print(averageRGB(i))
 
 if __name__ == '__main__':
     locations = getAllPaths(sys.argv[1])
@@ -61,3 +99,16 @@ if __name__ == '__main__':
         p.append(multiprocessing.Process(target=processImages, args=(splitLocations[i], num)))
         num += len(splitLocations[i])
         p[i].start()
+    for i in range(numThreads):
+        p[i].join()
+
+    writeCache(imageProcessed)
+
+    # colors = []
+    # start = time.time()
+    # locations = getAllPaths(imageProcessed)
+    # for i in locations:
+    #     if i.endswith(('.png', '.jpg', '.gif')):
+    #         colors.append(averageRGB(i))
+    # end = time.time()
+    # print("Runtime: ", end - start)
